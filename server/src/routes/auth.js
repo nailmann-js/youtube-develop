ec477bb440da88062d1635992e5e0be2a67919db
NailMannanov <mannanov.nai74@gmail.com>
2021-03-28 17:24:16 +0500

    add backend and few requests

---
+++ server/src/routes/auth.js
@@ -0,0 +1,50 @@
+import express, { response } from "express";
+import {PrismaClient} from '@prisma/client';
+import jwt from 'jsonwebtoken';
+import {protect} from "../middleware/authorization";
+const prisma = new PrismaClient();
+
+function getAuthRoutes() {
+  const router = express.Router();
+  // 'http://localhost:3001/api/v1/auth/current-user'
+  router.post('/google-login', googleLogin);
+  router.get('/me', protect, me);
+  return router;
+}
+
+// All controllers/utility functions here
+async function googleLogin(req, res) {
+  const {username, email} = req.body;
+
+  let user = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  });
+  if (!user) {
+    user = await prisma.user.create({
+      data:{
+        username, 
+        email,
+        avatar: ""
+      }
+    })
+  }
+
+  const tokenPayload = {id: user.id};
+  const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+  res.cookie('token', token, {httpOnly:true});
+  res.status(200).send(token);
+}
+
+async function me(req, res) {
+  console.log(req.user);
+
+  res.status(200).json({user: req.user});
+}
+
+function signout(req, res) {}
+
+export { getAuthRoutes };
